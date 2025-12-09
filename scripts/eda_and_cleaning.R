a28ab963ab92edd2ee669b9170c7774a73b83c5d
cmcgeehan <cmcgeehan@ithaca.edu>
2025-12-08 21:00:24 -0500

    Created r script instead of Rmd

---
+++ scripts/eda_and_cleaning.R
@@ -0,0 +1,11 @@
+# EDA and Data Cleaning
+
+# Data and Libraries
+athlete_data <- read.csv("..\\data\\athletes.csv")
+race_data <- read.csv("../data/races.csv")
+library(tidyverse)
+
+
+# Single-variable visualizations
+
+# Number of Races Run
